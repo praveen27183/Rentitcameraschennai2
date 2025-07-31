b1b13c9f3d2fe635c7b44784805b4b1d45eeb8ed
praveen <praveen271003.com>
2025-07-31 23:52:11 +0530

    Prepare for Vercel deployment

--- server/index.js
+++ server/index.js
@@ -4,10 +4,11 @@ import cors from 'cors';
 import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 import dotenv from 'dotenv';
+
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(cors());
